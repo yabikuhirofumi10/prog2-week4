693e51637fd8e4631efc13c717f62aebd9971cbd
yabikuhirohumi <e195762@ie.u-ryukyu.ac.jp>
2023-11-07 08:33:26 +0900

    test2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         //System.out.println("main method runs");
-        //git test
+        //git test 2
         Chatbot bot1 = new Chatbot();
         Chatbot bot2 = new Chatbot("yabiku"); 
         bot1.greeting();
